db8d1b82be9ee45bd7f844712bc015b7a23d62d8
postman17 <frompostal@gmail.com>
2020-10-21 20:44:34 +0500

    improve notification handler

--- rubicon/payments/views.py
+++ rubicon/payments/views.py
@@ -1,9 +1,11 @@
 import json
 import logging
+import urllib.parse
 from django.views.generic import TemplateView, View
 from django.http import JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.utils import timezone
 
 from . import models
 from . import services
@@ -41,16 +43,14 @@ class PaymentUrl(View):
 class OrderNotification(View):
     def post(self, request):
         logger.info(request.body)
-        try:
-            body = json.loads(request.body)
-        except Exception:
-            raise Http404
-
-        order = models.Order.objects.filter(id=body.get('MERCHANT_ORDER_ID')).first()
-        if order:
-            order.status = models.Order.STATUS_SUCCESS
-            order.updated_at = models.Order.STATUS_SUCCESS
-            order.save(update_fields=['status', 'updated_at'])
+        url_params = urllib.parse.parse_qs(request.body.decode())
+        merchant_id = url_params.get('MERCHANT_ORDER_ID')[0] if len(url_params.get('MERCHANT_ORDER_ID')) > 0 else 0
+        if merchant_id:
+            order = models.Order.objects.filter(id=merchant_id).first()
+            if order:
+                order.status = models.Order.STATUS_SUCCESS
+                order.updated_at = timezone.now()
+                order.save(update_fields=['status', 'updated_at'])
 
         raise Exception('YES')
 
